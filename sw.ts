cc6270fd2e4b4baa1420ed9bbb8da16a81085276
shiyangyang <shiyangyang@flowpp.com>
2024-05-05 21:06:00 +0800

    update sw.js

--- sw.ts
+++ sw.ts
@@ -1,19 +1,15 @@
 import { CACHE_KEY } from "./src/config";
 import { FetchEvent } from "./src/interface";
-import { publicPath } from './src/config'
 
-const RENAME = {
-    __OUTPUT_PATH__: (a = '') => a
-}
 const CACHES = [
-    publicPath,
-    RENAME.__OUTPUT_PATH__("./index.html"),
-    RENAME.__OUTPUT_PATH__("./manifest.json"),
-    RENAME.__OUTPUT_PATH__("./favicon.ico"),
-    RENAME.__OUTPUT_PATH__("./src/index.tsx"),
-    RENAME.__OUTPUT_PATH__("./img/frozen.css"),
-    RENAME.__OUTPUT_PATH__("./img/math.png"),
-    RENAME.__OUTPUT_PATH__("./img/study-48x48.png"),
+    '/',
+    "index.html",
+    "manifest.webmanifest",
+    "favicon.ico",
+    "static/index.js",
+    "img/frozen.css",
+    "img/math.png",
+    "img/study-48x48.png",
 ]
 
 self.addEventListener('install', function (_event) {
